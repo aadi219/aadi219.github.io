d012b76a8567853e6c640095566336ea3e1b249f
aadi219 <aaadibadola@gmail.com>
2024-08-14 02:07:38 -0400

    bug fix - make projects fill canvas

--- src/projects/components/Main.tsx
+++ src/projects/components/Main.tsx
@@ -5,7 +5,7 @@ const Main = () => {
     const { projects } = projectData;
 
     return (
-        <div className={`h-full px-2 relative`}>
+        <div className={`h-full w-full px-2 relative`}>
                 <ul id='projects' className={`grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-full gap-4 overflow-auto hide-scroll-complete`}>
                     {projects.map((p, idx) => {
                         return <li id={`${idx}`}  className="projectCard" key={idx}><ProjectCard project={p} /></li>
