7acf0dcbd8078a175a5095bcb2f342acee6e5f66
aadi219 <aadibadola1@gmail.com>
2026-07-09 11:39:05 -0400

    feat: add experience data and schema

---
+++ src/data/ExperienceData.ts
@@ -0,0 +1,9 @@
+export default interface ExperienceData {
+    role: string;
+    company: string;
+    location?: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+    details: string[];
+}
